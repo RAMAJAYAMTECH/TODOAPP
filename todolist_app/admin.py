c68ca81338b59aed66e9a19f23e4c15e49e536b7
RAMAJAYAMTECH <110171087+RAMAJAYAMTECH@users.noreply.github.com>
2022-10-14 14:51:19 +0530

    Update admin.py

--- todolist_app/admin.py
+++ todolist_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.db.models.fields.files import FileDescriptor
-from todolist_app.models import Tasklist,edit_page,gst_master,emp,work,client
+from todolist_app.models import Tasklist,edit_page,gst_master,emp,work,client,list
 # Register your models here.
 
 admin.site.register(Tasklist)
@@ -9,3 +9,4 @@ admin.site.register(gst_master)
 admin.site.register(client)
 admin.site.register(emp)
 admin.site.register(work)
+admin.site.register(list)
